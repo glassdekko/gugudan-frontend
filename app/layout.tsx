dc9f1d755636e327e09660c8c956e92247c19c0e
나큰솔 <knsol1992@e-hcg.com>
2025-12-16 09:16:21 +0900

    init: [나큰솔] Initial Commit
    -- eslint.config
    -- gitignore
    -- app/page
    -- package.json
    -- postcss.config
    -- tsconfig

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+// app/layout.tsx
+import './globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'MyApp',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
